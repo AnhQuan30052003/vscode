6279f46b3e9bd7bc098a529859dac952be1769ae
Nguyen Anh Quan <107738375+AnhQuan30052003@users.noreply.github.com>
2023-08-03 18:33:28 +0700

    Update do_thi_dinh_canh.cpp

--- src/language/C++/1.code/3.truong/1.toan roi rac/2.thuc hanh/Buoi 04 - tuan 10/do_thi_dinh_canh.cpp
+++ src/language/C++/1.code/3.truong/1.toan roi rac/2.thuc hanh/Buoi 04 - tuan 10/do_thi_dinh_canh.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
-using namespace std;
+using namespace std; 
 
 int main() {
-    
+  system("cls");
+  // Nhập dữ liệu
+  int dinh, canh;
+  cout << "Nhap so dinh: ";
+  cin >> dinh;
+  cout << "Nhap so canh: ";
+  cin >> canh;
+
+  // Xử lý
+  int c1, c2;
+  int a[dinh][dinh];
+  // Khỏi tạo đồ thì rỗng
+  for (int i = 0; i < dinh; i++) {
+    for (int j = 0; j < dinh; j++) {
+      a[i][j] = 0;
+    }
+  }
+
+  cout << "Nhap canh giua 2 dinh:" << endl;
+  for (int i = 1; i <= canh; i++) {
+    cin >> c1 >> c2;
+    a[c1-1][c2-1] = 1;
+    a[c2-1][c1-1] = 1;
+  }
+
+  // Ra dữ liệu
+  cout << "Ma tran dinh - canh:" << endl;
+  for (int i = 0; i < dinh; i++) {
+    for (int j = 0; j < dinh; j++) {
+      cout << a[i][j] << ' ';
+    }
+    cout << endl;
+  }
 }
