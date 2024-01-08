09a96d74e39e82b2a5c725aad419b08a877877f0
Admin <anhquan300503@gmail.com>
2024-01-08 09:50:57 +0700

    cập nhật

--- source/Web/temporary/a.js
+++ source/Web/temporary/a.js
@@ -1,7 +1,21 @@
+// (function($) {
+//   $('.box').click(function() {
+//     alert('Bạn đã click')
+//   })
+// })(jquery)
 
 $(function() {
-  $("#input-card").on("input", function() {
-    var text = $(this).val()
-    console.log(text)
+  $('.box').on('click', function() {
+    if ($(this).hasClass('bg-red')) {
+      alert('Chuyển sang màu xanh')
+      $(this).removeClass('bg-red')
+      $(this).addClass('bg-green')
+    }
+    else {
+      alert('Chuyển sang màu đỏ')
+      $(this).addClass('bg-red')
+      $(this).removeClass('bg-green')
+    }
   })
 })
+
