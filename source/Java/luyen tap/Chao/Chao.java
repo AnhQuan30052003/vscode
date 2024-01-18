324295197c224f33c63ef0d7b66f198c74c1165a
Admin <anhquan300503@gmail.com>
2024-01-18 10:08:48 +0700

    cập nhật

--- source/Java/luyen tap/Chao/Chao.java
+++ source/Java/luyen tap/Chao/Chao.java
@@ -2,6 +2,6 @@ package chao;
 
 public class Chao {
   public static void XinChao(String ten) {
-    System.out.println("Xin chao " + ten);
+    System.out.println("Xin chào " + ten);
   }
 }
--- source/Java/luyen tap/Program.java
+++ source/Java/luyen tap/Program.java
@@ -1,14 +1,23 @@
 import java.util.List;
+
+import chao.Chao;
+import toan.Toan;
+
 import java.util.ArrayList;
 
 public class Program {
   public static void main(String[] args) {
-    List<Integer> arr = new ArrayList<>(List.of(1, 2, 3, 4, 5));
+    // List<Integer> arr = new ArrayList<>(List.of(1, 2, 3, 4, 5));
 
-    arr.add(arr.size(), 10);
+    // arr.add(arr.size(), 10);
 
-    for (int a : arr) {
-      System.out.print(a + " ");
-    }
+    // for (int a : arr) {
+      // System.out.print(a + " ");
+    // }
+
+    // String ten = "Nguyễn Anh Quân";
+    // Chao.XinChao(ten);
+
+    System.out.print(Toan.BinhPhuong(3));
   }
 }
