588b0c617f078e3318c10761fff02a1d92209abc
Admin <anhquan300503@gmail.com>
2024-01-25 15:10:27 +0700

    cập nhật

--- source/C#/b/Program.cs
+++ source/C#/b/Program.cs
@@ -7,7 +7,7 @@ class Program {
     Console.InputEncoding = Encoding.Unicode;
     Console.OutputEncoding = Encoding.Unicode;
 
-    int[] a = new int[10000];
-    Console.Write(a[9999]);
+    string text = "Vẫn còn chạy được.";
+    Console.Write(text);
   }
 }
