6b385307c6f5fd22f9ee22b8339cf97ba534e555
Admin <anhquan300503@gmail.com>
2024-01-25 09:01:38 +0700

    cập nhật

--- source/Java/Main.java
+++ source/Java/Main.java
@@ -1,25 +1,8 @@
-import java.util.List;
-import java.util.ArrayList;
+
+import chao.Chao;
 
 public class Main {
   public static void main(String[] args) {
-    List<String> ds = new ArrayList<>();
-    ds.add("Anh Quân");
-    ds.add("Văn Đồng");
-
-    System.out.println("Danh sách hiện có:");
-    for (String _ds : ds) {
-      System.out.println(_ds);
-    }
-
-    System.err.println("Sau khi xoá một người: ");
-    for (int i = 0; i < ds.size(); i++) {
-      if (ds.get(i).equalsIgnoreCase("quân")) {
-        ds.remove(i);
-      }
-    }
-    for (String _ds : ds) {
-      System.err.println(_ds);
-    }
+    Chao.XinChao("Anh Quân");
   }
 }
