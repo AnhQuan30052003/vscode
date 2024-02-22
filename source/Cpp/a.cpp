52d404ed5a46aa652cf268c186856e19aea73300
Admin <anhquan300503@gmail.com>
2024-02-22 13:23:36 +0700

    cập nhật

--- source/Cpp/a.cpp
+++ source/Cpp/a.cpp
@@ -4,16 +4,7 @@ using namespace std;
 int main() {
   system("cls");
 
-  char a = 'a';
-  char b = 'b';
-  char c = 'c';
-
-  string s = "quan";
-  string t;
-  t += a;
-  t += b;
-  t += c;
-  s = t;
-  cout << t << endl;
-  cout << s << endl;
+  const int MAX = 5;
+  short led[MAX] = {8, 9, 10, 11, 12};
+  cout << led.size();
 }
