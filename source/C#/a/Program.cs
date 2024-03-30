b799443fb728a36bd52a1f871f538499a6f8d6fb
Admin <anhquan300503@gmail.com>
2024-03-30 16:58:32 +0700

    cập nhật

--- source/C#/a/Program.cs
+++ source/C#/a/Program.cs
@@ -1,2 +1,19 @@
-﻿Console.Clear();
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program {
+  static void Main() {
+    Console.Clear();
+    int[,] a = new int[10, 10];
+
+    Change.changeValue(a, 0, 2);
+
+    for (int i = 0; i < 10; i++) {
+      for (int j = 0; j < 10; j++) {
+        Console.Write($"{a[i, j]} ");
+      }
+      Console.WriteLine();
+    }
+  }
+
+  
+}
---
+++ source/C#/a/a.cs
@@ -0,0 +1,7 @@
+namespace System {
+  public class Change {
+    public static void changeValue(int[,] a, int x, int y) {
+      a[x, y] = 1;
+    }
+  }
+}
