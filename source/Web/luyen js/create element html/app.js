9316caa499bb6875def30072c1090d4255610ffd
Admin <anhquan300503@gmail.com>
2024-06-26 20:11:55 +0700

    cập nhật

--- source/Web/luyen js/create element html/app.js
+++ source/Web/luyen js/create element html/app.js
@@ -2,7 +2,7 @@ var _body = document.querySelector("body");
 var _div = document.createElement("div");
 var _input = document.createElement("input");
 
-_div.textContent = "Như bị khùng";
+// _div.textContent = "Như bị khùng";
 _body.appendChild(_div);
 _div.appendChild(_input)
 
@@ -13,3 +13,11 @@ _div.style.padding = "10px"
 _div.style.border = "solid 1px";
 // _div.style.color = "white";
 // _div.style.backgroundColor = "red";
+
+_input.setAttribute("value", "hello");
+
+_input.style.outline = "none";
+_input.style.padding = "5px 3px";
+_input.style.borderRadius = "5px";
+_input.style.border = "solid 1px";
+_input.style.fontSize = "14px";
