a7096ddb5d3037224ccc96da9e3554baccb3adc1
Admin <anhquan300503@gmail.com>
2024-09-09 12:16:07 +0700

    cập nhật

--- source/Python/practice/b.py
+++ source/Python/practice/b.py
@@ -1,4 +1,19 @@
 import os
+from random import randint as rand
 
-os.system('cls')
+os.system("cls" if os.name == "nt" else "clear")
 
+numbers = []
+
+for number in range(10):
+  number = rand(0, 9)
+  numbers.append(number)
+
+def isFirstEven():
+  for even in numbers:
+    if even % 2 == 0:
+      return even
+
+print(numbers)
+
+print(f"\nSố chẵn đầu tiên: {isFirstEven()}")
