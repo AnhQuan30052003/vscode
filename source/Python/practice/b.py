a82d1b787174ba7d1a46a5716fdb4160fc1866f5
Admin <anhquan300503@gmail.com>
2024-09-11 21:12:40 +0700

    cập nhật

--- source/Python/practice/b.py
+++ source/Python/practice/b.py
@@ -2,18 +2,4 @@ import os
 
 os.system("cls")
 
-open = [2, 3, 4, 5]
-close = [0, 1]
-find = [0, 4, 5, 6]
 
-print(find)
-
-for number in find:
-  print(f"Duyệt number = {number}: ", end="")
-  if number in open or number in close:
-    find.remove(number)
-    print("Xoá")
-  else:
-    print("Không xoá !")
-
-print(f"find sau khi lọc: {find}")
