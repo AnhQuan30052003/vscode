f7cfa1bb1f85c8e15ecab4c08f467444727a5800
Admin <anhquan300503@gmail.com>
2024-09-11 21:12:03 +0700

    cập nhật

--- source/Python/practice/BFS_DFS/code.py
+++ source/Python/practice/BFS_DFS/code.py
@@ -6,7 +6,6 @@ os.system("cls")
 # Khởi tạo
 listDinh = {}
 reverseListDinh = {}
-
 _open = []
 _close = []
 
@@ -16,14 +15,15 @@ matrix = []
 def setUpMatrix():
   global matrix
 
-  localPath = "\\source\\Python\\practice\\BFS_DFS\\matrix.txt"
+  localPath = "\source\Python\practice\BFS_DFS"
   path = os.getcwd() + localPath
+  nameFile = "\matrix.txt"
 
-  if not os.path.exists(path):
-    print("Không tìm thấy file đọc ma trận !")
+  if not os.path.exists(path + nameFile):
+    print("Lỗi: Không tìm thấy file đọc ma trận !")
     return
 
-  with open(path, "r") as file:
+  with open(path + nameFile, "r") as file:
     for line in file:
       listTemp = []
       for t in line.strip():
@@ -44,16 +44,15 @@ def setUpDict():
 
 def printRoadmap(diemBatDau: int, diemKetThuc: int):
   listResult = [diemKetThuc]
-  dinhXet = diemKetThuc
 
   i = 0
   while i < diemKetThuc:
-    if matrix[i][dinhXet] == 1:
+    if matrix[i][diemKetThuc] == 1:
       listResult.insert(0, i)
       if i == diemBatDau:
         break
 
-      dinhXet = diemKetThuc = i
+      diemKetThuc = i
       i = 0
     else:
       i += 1
@@ -61,7 +60,6 @@ def printRoadmap(diemBatDau: int, diemKetThuc: int):
   print(listDinh[0], end="")
   for i in range(1, len(listResult)):
     print(f" -> {listDinh[listResult[i]]}", end="")
-
 
 def main():
   global _open, _close
@@ -94,17 +92,18 @@ def main():
 
     # Tìm đỉnh tiềm năng từ đỉnh n
     dinhTiemNang = []
-    for i in range(1, len(matrix[n])):
+    for i in range(0, len(matrix[n])):
       if matrix[n][i] == 1:
         dinhTiemNang.append(i)
 
     # Loại bỏ các đỉnh tiềm năng đã có hoặc đã duyệt
+    dinhDaLoc = []
     for i in dinhTiemNang:
-      if i in _open or i in _close:
-        dinhTiemNang.remove(i)
+      if i not in _open and i not in _close:
+        dinhDaLoc.append(i)
 
     # Thêm đỉnh tiềm năng vào _open
-    _open += dinhTiemNang
+    _open += dinhDaLoc
 
   print("Không có đường đi !")
 
