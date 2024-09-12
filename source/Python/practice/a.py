3af7c51f13cb7e1b308a69807cf9e0cbe93d92ed
Admin <anhquan300503@gmail.com>
2024-09-12 19:39:52 +0700

    cập nhật

--- source/Python/practice/a.py
+++ source/Python/practice/a.py
@@ -1,15 +1,11 @@
+# Đếm xem những ký tự trong text xuất hiện bao nhiêu lần ?
 import os
-import array as arr
 
-# Lệnh xoá màn hình khi thông dịch chương trình
 os.system("cls" if os.name == "nt" else "clear")
 
-myDict = {
-  "ten": "Anh Quân",
-  "tuoi": 21
-}
+text = input("Text: ")
 
-myDict["ngheNghiep"] = "Sinh viên"
-
-for k, v in myDict.items():
-  print(f"{k} => {v}")
+for t in set(text):
+  count = text.count(t)
+  if t != ' ':
+    print(f"{count} {t}")
--- source/Python/practice/b.py
+++ source/Python/practice/b.py
@@ -2,12 +2,5 @@ import os
 
 os.system("cls")
 
-open = [1, 2, 3]
+text = "anh quan"
 
-open.pop(0)
-
-potential  = [4, 5]
-
-open = potential + open
-
-print(open)
