9b9efe68ab8119b08146e678651c5a772cd2e2a9
Admin <anhquan300503@gmail.com>
2024-09-23 12:27:47 +0700

    cập nhật

--- source/Python/practice/test/c.py
+++ source/Python/practice/test/c.py
@@ -1,27 +1,7 @@
-import os
+import os, time
 
-os.system("cls")
 
-text = ""
-result = ""
-k = 3
-
-pathFileRun = os.path.abspath(__file__)
-currentdir = os.path.dirname(pathFileRun)
-fileRead = "a.txt"
-fileWrite = "b.txt"
-
-path = os.path.join(currentdir, fileRead)
-with open(path, "r") as file:
-  for line in file:
-    text = line.strip()
-
-path = os.path.join(currentdir, fileWrite)
-with open(path, "w") as file:
-  typeChar = 97
-  for t in text:
-    maChar = (ord(t) - typeChar + k) % 26 + typeChar
-    char = chr(maChar)
-    result += char
-
-  file.write(result)
+for i in range(80, 100+1):
+  os.system("cls")
+  print(f"Tiến độ: {i}%")
+  time.sleep(1)
