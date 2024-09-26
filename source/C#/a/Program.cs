331c4a4cad8e83a75a664fd1e5fc1dd3700fc745
Admin <anhquan300503@gmail.com>
2024-09-26 11:43:34 +0700

    cập nhật

--- source/C#/a/Program.cs
+++ source/C#/a/Program.cs
@@ -3,6 +3,7 @@
 class Program {
   static void Main() {
     Console.Clear();
+    
     TraiCay tao = new TraiCay("Quả táo", new ThongTin("Đỏ", 12_000));
     Console.WriteLine(tao.ten);
     Console.WriteLine(tao.thongTin.mauSac);
