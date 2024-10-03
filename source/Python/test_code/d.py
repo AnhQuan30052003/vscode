d8806145643fa9ccb979ce975fa87704c6ec0cb4
Admin <anhquan300503@gmail.com>
2024-10-03 20:25:32 +0700

    cập nhật

--- source/Python/test_code/d.py
+++ source/Python/test_code/d.py
@@ -1,3 +1,27 @@
 import os
 
 os.system("cls")
+
+def decToBin(dec: int, length=8):
+  id_bin = bin(dec)[2:]
+  distance = length - len(id_bin)
+
+  text = ""
+  if distance > 0:
+    for i in range(distance):
+      text += "0"
+    text += id_bin
+
+  else:
+    text = id_bin
+
+  return text
+
+typeChar = 65
+
+K = 20
+
+for ass in range(65, 65 + 26):
+  P = ass - typeChar
+  xor = (P ^ K) % 26
+  print(f"{chr(P + typeChar)} -> {chr(xor + typeChar)}")
