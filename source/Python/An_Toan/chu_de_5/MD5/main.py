1fa2659b556cd946a9f615c4f79cfe1c44d96a15
= <=>
2024-10-12 09:43:16 +0700

    cập nhật

--- source/Python/An_Toan/chu_de_5/MD5/main.py
+++ source/Python/An_Toan/chu_de_5/MD5/main.py
@@ -1,4 +1,4 @@
-import os
+import os, hashlib
 import tkinter as tkt
 from tkinter import filedialog as fd, messagebox
 from Crypto.PublicKey import RSA
@@ -11,7 +11,7 @@ root = tkt.Tk()
 root.title("File MD5")
 
 window_width = 600
-window_height = 200
+window_height = 150
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
 
@@ -19,6 +19,83 @@ x = (screen_width // 2) - (window_width // 2)
 y = (screen_height // 2) - (window_height // 2)
 
 root.geometry(f"{window_width}x{window_height}+{x}+{y}")
+root.resizable(False, False)
+
+
+# Functions
+def openFile():
+  path = fd.askopenfilename()
+  if len(path) == 0:
+    return
+  
+  md5 = calculate_md5(path).upper()
+  
+  loadText(inputText, path)
+  loadText(outputText, md5, clock=True)
+
+# Thêm nôi dung text vào input
+def loadText(input: tkt.Text, text: str, clock: bool=False):
+  if clock:
+    input.config(state="normal")
+
+  input.delete(1.0, tkt.END)
+  input.insert(tkt.END, text)
+
+  if clock:
+    input.config(state="disabled")
+
+def calculate_md5(file_path):
+  md5_hash = hashlib.md5()
+
+  with open(file_path, "rb") as f:
+    for block in iter(lambda: f.read(4096), b""):
+      md5_hash.update(block)
+
+  return md5_hash.hexdigest()
+
+
+# UI
+styleText = {
+  "width": 50,
+  "wrap": "none",
+}
+
+styleTextPack = {
+  "side": "left",
+  "padx": 10,
+}
+
+styleLabel = {
+  "font": ("Arial", 12),
+  "width": 8,
+  "anchor": "w",
+}
+
+styleFrame = {
+  "padx": 30,
+}
+
+distanceY = 30
+
+# tren
+frameinput = tkt.Frame(root, width=window_width, height=30)
+frameinput.pack_propagate(False)
+frameinput.pack(**styleFrame, pady=(distanceY, 5))
+
+tkt.Label(frameinput, text="Chọn file", **styleLabel).pack(side="left")
+inputText = tkt.Text(frameinput, **styleText)
+inputText.pack(**styleTextPack)
+buttonChoose = tkt.Button(frameinput, text="...", padx=3, width=4, command=openFile)
+buttonChoose.pack(side="left")
+
+# duoi
+frameOutput = tkt.Frame(root, width=window_width, height=30)
+frameOutput.pack_propagate(False)
+frameOutput.pack(**styleFrame, pady=(5, distanceY))
+
+tkt.Label(frameOutput, text="MD5", **styleLabel).pack(side="left")
+outputText = tkt.Text(frameOutput, **styleText, state="disabled", bg="snow2")
+outputText.pack(**styleTextPack)
 
 
 # run
--- source/Python/An_Toan/chu_de_5/test/hash.py
+++ source/Python/An_Toan/chu_de_5/test/hash.py
@@ -3,18 +3,14 @@ import os
 import hashlib
 
 def calculate_md5(file_path):
-  # Khởi tạo đối tượng MD5
   md5_hash = hashlib.md5()
 
-  # Đọc file theo từng block (để tránh tốn bộ nhớ với file lớn)
   with open(file_path, "rb") as f:
     for block in iter(lambda: f.read(4096), b""):
       md5_hash.update(block)
 
-  # Trả về kết quả dưới dạng chuỗi hex
   return md5_hash.hexdigest()
 
-# Sử dụng hàm với file cần tính MD5
 file_path = "hash.py"
 file_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), file_path)
 
@@ -22,5 +18,4 @@ md5_value = calculate_md5(file_path)
 
 os.system("cls")
 print(f"MD5: {md5_value}")
-
-# 577e4ae29a34e5aff74609ca72dc7cbc
+print(f"len: {len(md5_value)}")
